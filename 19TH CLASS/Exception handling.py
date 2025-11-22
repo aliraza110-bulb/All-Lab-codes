867b7c81df9ed152cd737ccd05a91821cf876505
aliraza110-bulb <ali@thefivestarschool.org>
2025-11-22 23:17:29 +0500

    Updated exception handling and safe_divide

--- 19TH CLASS/Exception handling.py
+++ 19TH CLASS/Exception handling.py
@@ -41,51 +41,5 @@ finally:
 
 
 
-# def safe_divide(a,b):
-#         a=int(input('Enter The 1 value'))
-#         b=int(input('Enter The 2 value'))
-#     try:
-#         result=a/b
-#         return result
-#     except ZeroDivisionError:
-#         print('Error: CAnnot divide by zero')
-#         return None
-# return(safe_divide(a,b))
-
-
-
-
-
-
-
-import math
-
-
-
-def sqaure_root(number1):
-    try:
-        sqrt=math.sqrt(number1)
-        print('The square root is :',sqrt)
-    except ValueError:
-        print('Invalid input! Please enter a float or integer number and positive value.')
-while True:
-    number1=float(input('Enter The Value Of Number :'))
-    print(sqaure_root(number1))
-    exit_choice=('type exit to stop else enter')
-    if exit_choice == 'exit':
-        break
-    
-def complex_calc(num):
-    try:
-        result=num/(num-5)
-        print("the value after dividing is",result)
-    except Exception:
-        print("An error occured during calculation")
-while True:
-        num=int('enter The Value Of Num')
-        print(complex_calc(num))
-        exit_choice=('type exit to stop else enter')
-        if exit_choice == 'exit':
-            break
 
 
